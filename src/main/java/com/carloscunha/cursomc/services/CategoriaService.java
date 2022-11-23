3afca660248aadfc760ead153bf1182eade1ea17
Carlos Fernando Schmitt <carlosfernandoschmitt@hotmail.com>
2022-11-23 07:13:46 -0300

    Ajuste no endpoint /categorias/{id} e criação do tratamento de exeções.

--- src/main/java/com/carloscunha/cursomc/services/CategoriaService.java
+++ src/main/java/com/carloscunha/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.carloscunha.cursomc.domain.Categoria;
 import com.carloscunha.cursomc.repositories.CategoriaRepository;
+import com.carloscunha.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,7 +18,8 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", tipo: "+ Categoria.class.getName()));
 		
 	}
 
